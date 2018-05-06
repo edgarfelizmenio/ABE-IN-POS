6b00658c0bd782f684d356999038601fbbbc2a4f
Edgar Felizmenio <edgarfelizmenio@gmail.com>
2018-05-06 22:31:32 +0800

    updated tests

--- query_encounter_test.py
+++ query_encounter_test.py
@@ -21,64 +21,86 @@ input_filename = sys.argv[3]
 
 num_txns_str = input_filename.split("_")[0]
 
-users_file_name = 'users_{}_{}_{}.json'.format(policy_size, num_attributes, num_txns_str)
-encounter_ids_file_name = '{}_encounter_ids.json'.format(num_txns_str)
+encounter_ids_file_name = '{}_encounter_ids_{}_{}.json'.format(num_txns_str, policy_size, num_attributes)
 transaction_summary_file_name = '{}_{}_{}_query_transaction_summary.txt'.format(policy_size, num_attributes, num_txns_str)
 
-users_file_path = os.path.join(test_data_dir, users_file_name)
 input_path = os.path.join(test_data_dir, encounter_ids_file_name)
 transaction_summary_path = os.path.join(output_data_dir, transaction_summary_file_name)
 
-with open(users_file_path) as users_file:
-    users = json.load(users_file)
-
 with open(input_path) as input_file:
-    encounter_ids = json.load(input_file)
+    encounters = json.load(input_file)
     
 transaction_times = []
 status_codes = []
 
-def query(encounter_id, user): 
+transaction_times_success = []
+transaction_times = []
+status_codes = []
+error_count = 0
+
+def query(encounter_id, user, retry=False): 
+    start = time.time()
     payload = {
         'private_key': user['private_key']
     }
-    start = time.time()
     response = requests.post('{}/encounters/{}'.format(il_upstream_url, encounter_id),
                             headers=headers, 
                             auth=auth,
                             json=payload,
                             verify=False)
     end = time.time()
-    print(response.status_code)
-    #print(response.json()) 
-    status_codes.append(response.status_code)
     transaction_time = end - start
-    transaction_times.append(transaction_time)
+
+    if response.status_code == 200:
+        transaction_times_success.append(transaction_time)
+    else:
+        error_count += 1
+    if not retry:
+        status_codes.append(response.status_code)
+        transaction_times.append(transaction_time)
 
 num_encounters = 0
 with warnings.catch_warnings():
     warnings.simplefilter("ignore")
-    eupairs = zip(encounter_ids, users)
     test_start_date = datetime.datetime.utcnow()
-    for encounter_id, user in eupairs:
+    for encounter in encounters:
+        encounter_id = encounter['encounter_id']
+        user = encounter['user']
         query(encounter_id, user)
+    while error_count > 0:
+        retries = error_count
+        error_count = 0
+        while retries > 0:
+            encounter = random.choice(encounters)
+            encounter_id = encounter['encounter_id']
+            user = encounter['user']
+            query(encounter_id, user)
+            retries -= 1
     test_end_date = datetime.datetime.utcnow()
 
-print("number of encounters: {}".format(len(encounter_ids)))
+print("number of encounters: {}".format(len(encounters)))
 
 with open(transaction_summary_path, "w") as transaction_summary_file:
     total_time = sum(transaction_times)
     num_transactions = len(transaction_times)
     avg_txn_time = total_time/float(num_transactions)
     rate = 1/avg_txn_time
-    success_rate = reduce(lambda a,b: a + b, map(lambda x: 1 if x == 200 else 0, status_codes)) / len(status_codes)
+
+    total_time_success = sum(transaction_times_success)
+    avg_txn_time_success = total_time_success/float(num_transactions)
+    rate_success = 1/avg_txn_time_success
+
+    successful_transactions = reduce(lambda a,b: a + b, map(lambda x: 1 if x == 200 else 0, status_codes))
     transaction_summary_file.write('Test start: {}\n'.format(test_start_date))
     transaction_summary_file.write('Test end: {}\n'.format(test_end_date))
     transaction_summary_file.write('Total number of transactions: {}\n'.format(num_transactions))
     transaction_summary_file.write('Total time: {}\n'.format(total_time))
     transaction_summary_file.write('Average transaction time: {}\n'.format(avg_txn_time))
+    transaction_summary_file.write('Average transaction time (success): {}\n'.format(avg_txn_time_success))
     transaction_summary_file.write('Transactions per second: {}\n'.format(rate))
-    transaction_summary_file.write('Success Rate: {}\n'.format(success_rate))
+    transaction_summary_file.write('Transactions per second (success): {}\n'.format(rate_success))
+    transaction_summary_file.write('Successful transactions: {}\n'.format(successful_transactions))
     transaction_summary_file.write('\n')
     transaction_summary_file.writelines(["{}, {}\n".format(status_code, txn_time) for txn_time, status_code in zip(transaction_times, status_codes)])
-    
+    transaction_summary_file.write('\n')
+    transaction_summary_file.writelines(["{}, {}\n".format(200, txn_time) for txn_time in transaction_times_success])
--- save_encounter_test.py
+++ save_encounter_test.py
@@ -24,7 +24,7 @@ input_filename = sys.argv[3]
 num_txns_str = input_filename.split("_")[0]
 
 users_file_name = 'users_{}_{}_{}.json'.format(policy_size, num_attributes, num_txns_str)
-encounter_ids_file_name = '{}_encounter_ids.json'.format(num_txns_str)
+encounter_ids_file_name = '{}_encounter_ids_{}_{}.json'.format(num_txns_str, policy_size, num_attributes)
 transaction_summary_file_name = '{}_{}_{}_save_transaction_summary.txt'.format(policy_size, num_attributes, num_txns_str)
 
 users_file_path = os.path.join(test_data_dir, users_file_name)
@@ -39,11 +39,13 @@ with open(users_file_path) as users_file:
 with open(input_path) as input_file:
     encounters = json.load(input_file)
 
+transaction_times_success = []
 transaction_times = []
 status_codes = []
 encounter_ids = []
+error_count = 0
 
-def save(encounter, user): 
+def save(encounter, user, retry=False): 
     start = time.time()
     encounter['policy'] = user['policy']
     encounter['user_id'] = user['user_id']
@@ -54,17 +56,33 @@ def save(encounter, user):
                             verify=False)
     end = time.time()
     print(response.status_code) 
-    status_codes.append(response.status_code)
     transaction_time = end - start
-    transaction_times.append(transaction_time)
-    encounter_ids.append(response.json()['encounter_id'])
+
+    if response.status_code == 201:
+        transaction_times_success.append(transaction_time)
+        encounter_ids.append({
+            'user': user,
+            'encounter_id': response.json()['encounter_id']
+        })
+    else:
+        error_count += 1
+    if not retry:
+        status_codes.append(response.status_code)        
+        transaction_times.append(transaction_time)
 
 with warnings.catch_warnings():
     warnings.simplefilter("ignore")
-    eupair = zip(encounters, users)
+    eupair = list(zip(encounters, users))
     test_start_date = datetime.datetime.utcnow()
     for encounter, user in eupair:
         save(encounter, user)
+    while error_count > 0:
+        retries = error_count
+        error_count = 0
+        while retries > 0:
+            encounter, user = random.choice(eupair)
+            save(encounter, user, True)
+            retries -= 1
     test_end_date = datetime.datetime.utcnow()
 
 with open(encounter_ids_path, "w") as encounter_ids_file:
@@ -76,14 +94,22 @@ with open(transaction_summary_path, "w") as transaction_summary_file:
     num_transactions = len(transaction_times)
     avg_txn_time = total_time/float(num_transactions)
     rate = 1/avg_txn_time
-    success_rate = reduce(lambda a,b: a + b, map(lambda x: 1 if x == 201 else 0, status_codes)) / len(status_codes)
+
+    total_time_success = sum(transaction_times_success)
+    avg_txn_time_success = total_time_success/float(num_transactions)
+    rate_success = 1/avg_txn_time_success
+
+    successful_transactions = reduce(lambda a,b: a + b, map(lambda x: 1 if x == 201 else 0, status_codes))
     transaction_summary_file.write('Test start: {}\n'.format(test_start_date))
     transaction_summary_file.write('Test end: {}\n'.format(test_end_date))
     transaction_summary_file.write('Total number of transactions: {}\n'.format(num_transactions))
     transaction_summary_file.write('Total time: {}\n'.format(total_time))
     transaction_summary_file.write('Average transaction time: {}\n'.format(avg_txn_time))
+    transaction_summary_file.write('Average transaction time (success): {}\n'.format(avg_txn_time_success))
     transaction_summary_file.write('Transactions per second: {}\n'.format(rate))
-    transaction_summary_file.write('Success Rate: {}\n'.format(success_rate))
+    transaction_summary_file.write('Transactions per second (success): {}\n'.format(rate_success))
+    transaction_summary_file.write('Successful transactions: {}\n'.format(successful_transactions))
     transaction_summary_file.write('\n')
     transaction_summary_file.writelines(["{}, {}\n".format(status_code, txn_time) for txn_time, status_code in zip(transaction_times, status_codes)])
-    
+    transaction_summary_file.write('\n')
+    transaction_summary_file.writelines(["{}, {}\n".format(201, txn_time) for txn_time in transaction_times_success])
